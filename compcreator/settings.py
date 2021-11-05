c0121ceccc935176357d48ae29262d95efaabc7d
hhuumm <h4mid@Hotmail.com>
2021-11-05 15:37:36 -0400

    Adding styling to the comp creator

--- compcreator/settings.py
+++ compcreator/settings.py
@@ -96,9 +96,7 @@ WSGI_APPLICATION = 'compcreator.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('NAME'),
-        'USER':env('USER'),
-        'PASSWORD':env('PASSWORD'),
+        'NAME': 'computercreator',
     }
 }
 
