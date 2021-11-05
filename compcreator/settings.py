3713552e13e4e28fcfb353cf55d1cc89b7b491ec
hhuumm <h4mid@Hotmail.com>
2021-11-05 15:48:44 -0400

    Removed final environ reference

--- compcreator/settings.py
+++ compcreator/settings.py
@@ -18,8 +18,7 @@ import os,sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Initialise environment variables
-environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
+
 # Take environment variables from .env file
 
 
